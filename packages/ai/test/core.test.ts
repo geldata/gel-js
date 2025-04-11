97889790284b09b1707e27f5590529eb5d5fced0
Scott Trinh <scott@scotttrinh.com>
2025-04-11 09:38:20 -0400

    Check available extensions first
    Only run AI tests of `ai` extension is available.

--- packages/ai/test/core.test.ts
+++ packages/ai/test/core.test.ts
@@ -1,18 +1,21 @@
 import { type Client } from "gel";
 import { createRAGClient } from "../dist/index.js";
-import { getClient, waitFor } from "@repo/test-utils";
+import { getClient, waitFor, getAvailableExtensions } from "@repo/test-utils";
 import { createMockHttpServer, type MockHttpServer } from "./mockHttpServer";
 
-let mockServer: MockHttpServer;
+const availableExtensions = getAvailableExtensions();
 
-beforeAll(async () => {
-  // Start the mock server
-  mockServer = createMockHttpServer();
+if (availableExtensions.has("ai")) {
+  let mockServer: MockHttpServer;
 
-  const client = getClient();
-  await client.ensureConnected();
-  try {
-    await client.execute(`
+  beforeAll(async () => {
+    // Start the mock server
+    mockServer = createMockHttpServer();
+
+    const client = getClient();
+    await client.ensureConnected();
+    try {
+      await client.execute(`
 create extension pgvector;
 create extension ai;
 
@@ -46,97 +49,98 @@ configure current branch insert ext::ai::CustomProviderConfig {
 
 configure current branch set ext::ai::Config::indexer_naptime := <duration>"100ms";
     `);
-  } finally {
-    await client.close();
-  }
-}, 25_000);
+    } finally {
+      await client.close();
+    }
+  }, 25_000);
 
-afterAll(async () => {
-  // Stop the mock server
-  if (mockServer) {
-    await mockServer.close();
-  }
-});
-
-describe("@gel/ai", () => {
-  let client: Client;
-  beforeEach(() => {
-    mockServer.resetRequests();
+  afterAll(async () => {
+    // Stop the mock server
+    if (mockServer) {
+      await mockServer.close();
+    }
   });
 
-  afterEach(async () => {
-    await client?.close();
-  });
-
-  test("RAG query", async () => {
-    client = getClient({
-      tlsSecurity: "insecure",
+  describe("@gel/ai", () => {
+    let client: Client;
+    beforeEach(() => {
+      mockServer.resetRequests();
     });
-    await client.execute(`
+
+    afterEach(async () => {
+      await client?.close();
+    });
+
+    test("RAG query", async () => {
+      client = getClient({
+        tlsSecurity: "insecure",
+      });
+      await client.execute(`
 insert Astronomy { content := 'Skies on Mars are red' };
 insert Astronomy { content := 'Skies on Earth are blue' };
       `);
-    await waitFor(async () =>
-      expect(mockServer.getEmbeddingsRequests().length).toBe(1),
-    );
+      await waitFor(async () =>
+        expect(mockServer.getEmbeddingsRequests().length).toBe(1),
+      );
 
-    const ragClient = createRAGClient(client, {
-      model: "text-generation-test",
-    }).withContext({
-      query: "select Astronomy",
-    });
+      const ragClient = createRAGClient(client, {
+        model: "text-generation-test",
+      }).withContext({
+        query: "select Astronomy",
+      });
 
-    const result = await ragClient.queryRag({
-      prompt: "What color are the skies on Mars?",
-    });
+      const result = await ragClient.queryRag({
+        prompt: "What color are the skies on Mars?",
+      });
 
-    expect(result).toEqual("This is a mock response.");
+      expect(result).toEqual("This is a mock response.");
 
-    const streamedResult = ragClient.streamRag({
-      prompt: "What color are the skies on Mars?",
-    });
+      const streamedResult = ragClient.streamRag({
+        prompt: "What color are the skies on Mars?",
+      });
 
-    let streamedResultString = "";
-    for await (const message of streamedResult) {
-      if (
-        message.type === "content_block_start" &&
-        message.content_block.type === "text"
-      ) {
-        streamedResultString += message.content_block.text;
-      } else if (
-        message.type === "content_block_delta" &&
-        message.delta.type === "text_delta"
-      ) {
-        streamedResultString += message.delta.text;
+      let streamedResultString = "";
+      for await (const message of streamedResult) {
+        if (
+          message.type === "content_block_start" &&
+          message.content_block.type === "text"
+        ) {
+          streamedResultString += message.content_block.text;
+        } else if (
+          message.type === "content_block_delta" &&
+          message.delta.type === "text_delta"
+        ) {
+          streamedResultString += message.delta.text;
+        }
       }
-    }
 
-    expect(streamedResultString).toEqual("This is a mock response.");
-  }, 25_000);
+      expect(streamedResultString).toEqual("This is a mock response.");
+    }, 25_000);
 
-  test("embedding request", async () => {
-    client = getClient({
-      tlsSecurity: "insecure",
+    test("embedding request", async () => {
+      client = getClient({
+        tlsSecurity: "insecure",
+      });
+      const ragClient = createRAGClient(client, {
+        model: "text-generation-test",
+      });
+
+      const result = await ragClient.generateEmbeddings({
+        inputs: ["beep boop!"],
+        model: "text-embedding-test",
+      });
+
+      await waitFor(
+        async () => expect(mockServer.getEmbeddingsRequests().length).toBe(1),
+        10_000,
+        500,
+      );
+
+      expect(result).toEqual(
+        // The number of occurences of the first ten letters of the alphabet.
+        // two 'b's and two 'e's.
+        [0, 2, 0, 0, 2, 0, 0, 0, 0, 0],
+      );
     });
-    const ragClient = createRAGClient(client, {
-      model: "text-generation-test",
-    });
-
-    const result = await ragClient.generateEmbeddings({
-      inputs: ["beep boop!"],
-      model: "text-embedding-test",
-    });
-
-    await waitFor(
-      async () => expect(mockServer.getEmbeddingsRequests().length).toBe(1),
-      10_000,
-      500,
-    );
-
-    expect(result).toEqual(
-      // The number of occurences of the first ten letters of the alphabet.
-      // two 'b's and two 'e's.
-      [0, 2, 0, 0, 2, 0, 0, 0, 0, 0],
-    );
   });
-});
+}
